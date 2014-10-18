14258c72c29ff2027232ab1bf7d9e86312f713c1
arnolf <afayolle@****.***>
2014-10-18 15:21:21 +0200

    add validation for mongotextfield

--- tapestry-mongo-module/src/main/java/org/arnolf/tapestry/mongo/components/MongoTextField.java
+++ tapestry-mongo-module/src/main/java/org/arnolf/tapestry/mongo/components/MongoTextField.java
@@ -3,11 +3,13 @@ package org.arnolf.tapestry.mongo.components;
 import org.apache.tapestry5.BindingConstants;
 import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.FieldTranslator;
+import org.apache.tapestry5.FieldValidator;
 import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.corelib.components.TextField;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.FieldTranslatorSource;
+import org.apache.tapestry5.services.FieldValidatorSource;
 
 public class MongoTextField extends AbstractMongoField {
 
@@ -18,12 +20,22 @@ public class MongoTextField extends AbstractMongoField {
 	private FieldTranslatorSource fieldTranslatorSource;
 	
 	@Inject
+	private FieldValidatorSource fieldValidatorSource;
+	
+	@Inject
 	private ComponentResources componentResources;
 	
 	@Parameter(allowNull = false, required = true, defaultPrefix = BindingConstants.LITERAL)
 	private String translate;
 	
+	@Parameter(allowNull = false, required = true, defaultPrefix = BindingConstants.LITERAL)
+	private String validate;
+	
 	public FieldTranslator<?> getTranslate() {	
 		return fieldTranslatorSource.createTranslator(((org.apache.tapestry5.runtime.Component)mongoField).getComponentResources(), translate);
+	}
+	
+	public FieldValidator<?> getValidate() {
+		return fieldValidatorSource.createValidators(mongoField, validate);
 	}
 }
